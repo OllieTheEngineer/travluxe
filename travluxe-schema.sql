f10598bcbf0ee8a308c8672e1af6ed47c7a19eaf
ollie <olivinanzali07@gmail.com>
2023-04-06 17:35:09 -0400

    updated schema and fixed errors

--- travluxe-schema.sql
+++ travluxe-schema.sql
@@ -14,27 +14,27 @@ CREATE TABLE users (
 
 CREATE TABLE city (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    city_name TEXT NOT NULL,
     country TEXT NOT NULL,
     timezone TEXT NOT NULL,
-    time INTEGER,
-    user_username VARCHAR REFERENCE users(username) ON DELETE CASCADE
+    timeofday TEXT NOT NULL,
+    user_username VARCHAR REFERENCES users(username) ON DELETE CASCADE
 );
 
 CREATE TABLE weather (
     id SERIAL PRIMARY KEY,
-    temperature DECIMAL(),
+    temperature TEXT NOT NULL,
     conditions TEXT NOT NULL,
     wind_speed DECIMAL,
     user_username VARCHAR REFERENCES users(username) ON DELETE CASCADE,
     city_id INTEGER REFERENCES city(id)
-)
+);
 
 CREATE TABLE restaurants (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    rating DECIMAL(2,5),
-    address TEXT NOT NULL,
+    restaurant_name TEXT NOT NULL,
+    rating TEXT NOT NULL,
+    restaurant_address TEXT NOT NULL,
     phone TEXT NOT NULL,
     image_url TEXT NOT NULL,
     user_username VARCHAR REFERENCES users(username) ON DELETE CASCADE,
@@ -43,10 +43,10 @@ CREATE TABLE restaurants (
 
 CREATE TABLE touristic_sites (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    tour_name TEXT NOT NULL,
     website TEXT NOT NULL,
-    rating DECIMAL(2,5),
-    adress TEXT NOT NULL,
+    rating TEXT NOT NULL,
+    tour_address TEXT NOT NULL,
     phone TEXT NOT NULL,
     image_url TEXT NOT NULL,
     city_id INTEGER REFERENCES city(id) ON DELETE CASCADE
@@ -58,21 +58,21 @@ VALUES ('OllieBoop', 'Travel2023', 'Ollie', 'Boop', 'ollieboop@example.com'),
        ('GaetanJR', 'Travel2023', 'Gaetan', 'Mandeng', 'GaetanM@example.com');
 
 -- Insert sample cities
-INSERT INTO city (name, country, timezone, time, user_username)
-VALUES ('Paris', 'France', 'Europe/Paris', 7200, 'johndoe'),
-       ('Barcelona', 'Spain', 'Europe/Madrid', 7200, 'janedoe');
+INSERT INTO city (city_name, country, timezone, timeofday, user_username)
+VALUES ('Paris', 'France', 'Europe/Paris', 7200, 'OllieBoop'),
+       ('Barcelona', 'Spain', 'Europe/Madrid', 7200, 'GaetanJR');
 
 -- Insert sample weather data
 INSERT INTO weather (temperature, conditions, wind_speed, user_username, city_id)
-VALUES (23.5, 'Sunny', 15.6, 'johndoe', 1),
-       (17.8, 'Cloudy', 8.3, 'janedoe', 2);
+VALUES (23.5, 'Sunny', 15.6, 'OllieBoop', 1),
+       (17.8, 'Cloudy', 8.3, 'GaetanJR', 2);
 
 -- Insert sample restaurants
-INSERT INTO restaurants (name, rating, address, phone, image_url, user_username, city_id)
-VALUES ('Chez Pierre', 4.5, '12 Rue de la Paix, 75002 Paris', '+33 1 42 61 68 29', 'https://example.com/chez-pierre.jpg', 'johndoe', 1),
-       ('El Xampanyet', 4.3, 'Carrer de Montcada, 22, 08003 Barcelona', '+34 933 19 30 33', 'https://example.com/el-xampanyet.jpg', 'janedoe', 2);
+INSERT INTO restaurants (restaurant_name, rating, restaurant_address, phone, image_url, user_username, city_id)
+VALUES ('Chez Pierre', 4.5, '12 Rue de la Paix, 75002 Paris', '+33 1 42 61 68 29', 'https://example.com/chez-pierre.jpg', 'OllieBoop', 1),
+       ('El Xampanyet', 4.3, 'Carrer de Montcada, 22, 08003 Barcelona', '+34 933 19 30 33', 'https://example.com/el-xampanyet.jpg', 'GaetanJR', 2);
 
 -- Insert sample touristic sites
-INSERT INTO touristic_sites (name, website, rating, address, phone, image_url, city_id)
+INSERT INTO touristic_sites (tour_name, website, rating, tour_address, phone, image_url, city_id)
 VALUES ('Eiffel Tower', 'https://www.toureiffel.paris/', 4.7, '5 Avenue Anatole France, 75007 Paris', '+33 892 70 12 39', 'https://example.com/eiffel-tower.jpg', 1),
        ('La Sagrada Familia', 'https://sagradafamilia.org/en', 4.6, 'Carrer de Mallorca, 401, 08013 Barcelona', '+34 932 08 04 14', 'https://example.com/sagrada-familia.jpg', 2);
